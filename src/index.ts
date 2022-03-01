bda67f206e7df1be68e79932f3a977ebe222a507
Thiago Vinicius Santos <thiago@localhost.localdomain>
2022-03-01 10:04:25 -0300

    Procfile

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { routes } from "./routes/encomenda.routes";
+const PORT = process.env.PORT || 3333;
 
 const app = express();
 
 app.use(express.json());
 app.use("/encomendas", routes);
 
-app.listen(3333, () => {
+app.listen(PORT, () => {
   console.log("🚀 Server is running");
 });
