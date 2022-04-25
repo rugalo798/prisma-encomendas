68a8cbe3f447f05598201c95ec4286b59afa2d17
rugalo798 <34358578+rugalo798@users.noreply.github.com>
2022-04-25 15:31:14 -0300

    comments

--- src/controllers/EncomendaController.ts
+++ src/controllers/EncomendaController.ts
@@ -2,9 +2,12 @@ import { Request, Response } from "express";
 import { prismaClient } from "../database/prismaClient";
 
 export class EncomendaController {
+
+  // Metodo de cadastro de encomendas
   async create(request: Request, response: Response): Promise<Response> {
     const { name, tracking_code } = request.body;
 
+    // Verificação se a encomenda já existe
     const encoAlreadyExists = await prismaClient.encomenda.findUnique({
       where: {
         tracking_code,
@@ -15,6 +18,7 @@ export class EncomendaController {
       return response.status(401).json({ Error: "Essa Encomenda já Existe!" });
     }
 
+    // Se a encomenda não existir, crie
     const encomenda = await prismaClient.encomenda.create({
       data: {
         name,
@@ -22,15 +26,18 @@ export class EncomendaController {
       },
     });
 
+    // Retornando se a encomenda foi criada com um formato JSON
     return response.json(encomenda);
   }
 
+  // Metodo para retornar todas as encomendas cadastradas em um JSON
   async getAll(request: Request, response: Response): Promise<Response> {
     const result = await prismaClient.encomenda.findMany();
 
     return response.json(result);
   }
 
+  // Metodo para retornar todas as encomendas com status de entregue em um JSON
   async getSent(request: Request, response: Response): Promise<Response> {
     const result = await prismaClient.encomenda.findMany({
       where: {
